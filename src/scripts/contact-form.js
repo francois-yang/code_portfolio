d7e22626a0b7d505178da9312ae509e2072aaf91
francois-yang <francois.yang@edu.univ-fcomte.fr>
2025-11-19 23:12:16 +0100

    formulaire coté client maj

--- src/scripts/contact-form.js
+++ src/scripts/contact-form.js
@@ -25,7 +25,7 @@ export function initContactForm() {
     
     try {
       const pb = new PocketBase('https://portfolio.fryg.fr');
-      await pb.collection('contacts').create(data);
+      await pb.collection('contact').create(data);
       
       formMessage.textContent = 'Message envoyé avec succès !';
       formMessage.className = 'bg-turquoise/20 text-turquoise rounded-lg p-4 text-center';

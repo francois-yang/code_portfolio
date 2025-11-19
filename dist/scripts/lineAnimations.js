b9fb9f4f1a2f0be07ed02ed55d363b1666fbe2e9
francois-yang <francois.yang@edu.univ-fcomte.fr>
2025-11-19 16:57:54 +0100

    deploy 8

--- dist/scripts/lineAnimations.js
+++
@@ -1,8 +0,0 @@
-// Animation des lignes
-export function initLineAnimations() {
-    setTimeout(() => {
-        document.querySelectorAll(".line-anim").forEach((svg) => {
-            svg.classList.add("revealed");
-        });
-    }, 300);
-}
